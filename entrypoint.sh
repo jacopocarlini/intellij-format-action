b6c79860671adf547c82a6838586f1e48fca5fff
Jacopo <jacopo.carlini@gmail.com>
2023-06-23 22:55:04 +0200

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Wrapper for the formatter that passes action args and processes the output.
-# Required args:
-# - Path to base directory.
-# - File include glob pattern.
-# - Whether to fail on file changes.
-
-if [[ $# -ne 4 ]]; then
+if [[ $# -ne 5 ]]; then
   echo 'Exactly three parameters (base dir path, input file pattern, fail on changes, auto fix) required.'
   exit 1
 fi
 
-base_path=$1
-include_pattern=$2
-fail_on_changes=$3
-auto_fix=$4
+github_ref=$1
+base_path=$2
+include_pattern=$3
+fail_on_changes=$4
+auto_fix=$5
 
 # Prepare the workspace for safe usage:
 git config --global --add safe.directory /github/workspace
@@ -43,7 +38,8 @@ fi
 if [[ "$auto_fix" == 'true' ]]; then
   git config --global user.name 'Your Name'
   git config --global user.email 'your-username@users.noreply.github.com'
-  git commit -am "IntelliJ Formatting code"
-  git push
+  git add .
+  git commit -m "IntelliJ Formatting code"
+  git push origin HEAD:$github_ref
 fi
 
