149a3473e7a72c980df119425387caadc5ff6487
Jacopo <jacopo.carlini@gmail.com>
2023-06-24 19:04:49 +0200

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -eu
 
 /jb/idea/bin/inspect.sh ${GITHUB_WORKSPACE}/ ${GITHUB_WORKSPACE}/.idea/inspectionProfiles/Project_Default.xml ${GITHUB_WORKSPACE}/target/idea_inspections -v2
 
-/analyze_inspections.py -i ${GITHUB_WORKSPACE}/target/idea_inspections
+#/analyze_inspections.py -i ${GITHUB_WORKSPACE}/target/idea_inspections
